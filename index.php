2abab9eadf34b7bbea48eaa7e7d0e70220ac4bb6
Nishkarsh Sinoriya <nishkarshsinoriya@gmail.com>
2018-10-06 09:33:25 -0700

    MULTIPLE_ADDRESS
    contactus (Green)

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="custom.css">
+    <title></title>
+  </head>
+  <body>
+    <div id="contact">
+        <div class="container">
+          <div class="row">
+              <div class="col-sm-10 col-sm-offset-1">
+                        <div class="card">
+                          <div class="container-fluid">
+                            <div class="row">
+                              <div class="col-sm-8">
+                                <div class="wrapper">
+                                  <div class="heading">
+                                    <h3>contact us</h3>
+                                  </div>
+                                  <h3>other ways to connect</h3>
+                                    <div class="container-fluid">
+                                      <div class="row">
+                                        <div class="content">
+                                          <div class="col-xs-12">
+                             <div class="info">
+                               <h3><span ><img src="img/call.png" alt=""></span>0751-2483760</h3>
+                             </div>
+                         </div>
+                           <div class="col-xs-12">
+                             <div class="info">
+                               <h3><span ><img src="img/smartphone.png" alt=""></span>+1-202-555-0163</h3>
+                             </div>
+                         </div>
+                         <div class="col-xs-12">
+                           <div class="info">
+                             <h3><span ><img src="img/envelope.png" alt=""></span>noreply@lifeatleo.org</h3>
+                           </div>
+                         </div>
+                         <div class="col-xs-12">
+                           <div class="info">
+                             <h3><span ><img src="img/placeholder.png" alt=""></span>Koteshwar Tiraha, Koteshwar Rd, Fort View Colony, Gwalior, Madhya Pradesh 474003.</h3>
+                           </div>
+                     </div>
+                     <div class="row">
+                            <iframe class="contact_map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14316.066062774216!2d78.164138!3d26.2286529!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4e3050b3d5617d88!2sSaraswati+Shishu+Gyan+Mandir!5e0!3m2!1sen!2sin!4v1509029108850" frameborder="0" style="border:0" allowfullscreen></iframe>
+                          </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                  <div class="container-fluid">
+                                    <div class="row">
+                                      <div class="location">
+                                        <div class="col-xs-3">
+                                            <div class="locationb">
+                                          <div class="container-fluid">
+                                            <div class="row">
+                                                <h3>gwalior</h3>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-xs-3">
+                                          <div class="locationb">
+                                          <div class="container-fluid">
+                                            <div class="row">
+                                                <h3>dehli</h3>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-xs-3">
+                                          <div class="locationb">
+                                          <div class="container-fluid">
+                                            <div class="row">
+                                                <h3>gurgaon</h3>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-xs-3">
+                                          <div class="locationb">
+                                          <div class="container-fluid">
+                                            <div class="row">
+                                                <h3>morena</h3>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+
+                                    </div>
+                                  </div>
+                                </div>
+                            </div>
+
+                              <div class="col-sm-4">
+                                <div class="wrap">
+                                  <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                          <div class="text">
+                                            <h3>connect</h3>
+                                          </div>
+                                        </div>
+                                            <div class="col-xs-12">
+                                              <div class="icon">
+                                                <img class="imageshift" src="img/facebook.png" alt="">
+                                                <img  class="image"src="img/twitter.png" alt="">
+                                                <img class="image" src="img/in.png" alt="">
+                                                <img class="image" src="img/google.png" alt="">
+                                                <img class="image" src="img/insta.png" alt="">
+                                              </div>
+                                          </div>
+                                            <div class="col-xs-12">
+                                        <form class="" action="index.html" method="post">
+                                          <div class="grid">
+                                            <h3 class="head">send us a message</h3>
+                                            <div class="col-xs-12">
+                                              <div class="input_wrapper">
+                                                <label for="">name</label>
+                                                <input class="input" type="text" name="" value="">
+                                              </div>
+                                            </div>
+                                            <div class="col-xs-12">
+                                              <div class="input_wrapper">
+                                                <label for="">contact no</label>
+                                                <input  class="input" type="text" name="" value="">
+                                              </div>
+                                            </div>
+                                            <div class="col-xs-12">
+                                              <div class="input_wrapper">
+                                                  <label for="">email</label>
+                                                <input  class="input" type="text" name="" value="">
+                                              </div>
+                                              </div>
+                                            <div class="col-xs-12">
+                                              <div class="input_wrapper">
+                                                <label for="">message</label>
+                                              <textarea class="input" name="name" rows="7" ></textarea>
+                                              </div>
+                                            </div>
+                                            <div class="col-xs-12">
+                                              <div class="buttons_pane">
+                                              <button class="button"type="button" name="button">send</button>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </form>
+                                          </div>
+
+
+                                    </div>
+                                  </div>
+                              </div>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+</html>
